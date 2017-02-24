a263cff5c3440060fcadc41f9b378b13734f7dd6
M_GRAD <YoUrm0m!>
2017-02-24 14:55:05 -0800

    added some file io stuff

--- pa1.c
+++ pa1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+// Function prototypes
+
+int main(int argc, char *argv[]){
+
+	if(argc !=2){
+		printf("Program needs one input <filename>");
+		exit(1);
+	}
+
+	File *infile = fopen(argv[1], "r");
+	if(infile ==NULL){
+		fprintf(stderr, "Error: Could not open file %s for reading\n", argv[1]);
+		exit(1);
+	}
+
+}
