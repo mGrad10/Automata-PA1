a43d2f3596564039ef7a32736991d67377292e5c
M_GRAD <YoUrm0m!>
2017-02-28 13:48:18 -0800

    the beginning

--- pa1.c
+++ pa1.c
@@ -26,10 +26,33 @@ int main(int argc, char *argv[]){
 
 	// Read first line containing number of states
 	int num_states = 0;
-
 	fscanf(infile, "%d", &num_states);
-
 	printf("Number of states: %d\n", num_states);
 
-	//Correctly reads in file :-) 
+	//Gettting the alphabet
+	char *alpha = NULL; //TODO: How to init this?
+	fscanf(infile, "%s", alpha);
+	int num_alpha = strlen(alpha);
+
+	int num_transitions = num_alpha * num_states;
+	int transition_func[num_alpha][num_states];
+
+	for(int i = 0; i< num_transitions; i++){
+		int curr_state = 0;
+		int next_state = 0;
+		char input;
+		fscanf(infile, "%d '%c' %d", &curr_state, &input, &next_state);
+	}
+
+	//TODO: Read in the transitions (for loop with num_transitions)
+	// Store in 2-d array(num_statesX num_alpha)
+
+	//TODO: Read in start state
+
+	//TODO: Read in accept states (store in an array)
+
+	//TODO: Read in (for loop) the strings
+
+	//TODO: 
+
 }
